5c6b1e338bd7758291afd2da51d9e21cc079fea3
tanvirch0750 <tanvir0750@yahoo.com>
2020-11-09 20:33:51 +0600

    complete the responsive and added preloader

--- js/main.js
+++ js/main.js
@@ -331,3 +331,11 @@ function bodyScrollingToggle(){
   })
 
 })();
+
+// FREELOADER
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display="none";
+  },600)
+})
